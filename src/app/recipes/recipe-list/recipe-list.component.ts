e3bb61b095520fe1a952eacd9795f3b451652b50
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-02-05 14:38:02 +0200

    front end styling for recipes list and item and shopping list and item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes : Recipe[] = [];
+  recipes : Recipe[] = [
+    new Recipe('Test Recipe',
+    'This is a description for recipe',
+     'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-08-how-to-juiciest-turkey-meatballs%2FHow-to-Make-the-Best-Juiciest-Turkey-Meatballs_055'
+     ),
+    new Recipe('Test Recipe',
+    'This is a description for recipe',
+     'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-08-how-to-juiciest-turkey-meatballs%2FHow-to-Make-the-Best-Juiciest-Turkey-Meatballs_055'
+     ),
+    new Recipe('Test Recipe',
+    'This is a description for recipe',
+     'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-08-how-to-juiciest-turkey-meatballs%2FHow-to-Make-the-Best-Juiciest-Turkey-Meatballs_055'
+     ),
+  ];
   constructor() { }
 
   ngOnInit() {
