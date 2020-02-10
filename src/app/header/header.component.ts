7f683158b9105566803e607fecc3b6c841f41635
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-02-10 10:39:07 +0200

    added navigation through *ngIf and event binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
-    selector:'app-header',
-    templateUrl:'header.component.html',
-    styleUrls:['header.component.css']
+    selector: 'app-header',
+    templateUrl: 'header.component.html',
+    styleUrls: ['header.component.css']
 
 })
-export class HeaderComponent{
+export class HeaderComponent {
     collapsed = true;
+    @Output() featureSelected = new EventEmitter<{}>();
+    onSelect(feature: string) {
+        this.featureSelected.emit(feature);
+    }
 }
