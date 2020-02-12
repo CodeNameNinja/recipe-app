511c937d758971b4ae8e9d60b9fa00efed542be7
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-02-12 15:20:57 +0200

    added a shopping list service and recipe service and moved the array of recipes from the recipes-list component to the recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,28 +9,13 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Meat Balls',
-    'This is a recipe for my famous meat balls',
-     // tslint:disable-next-line: max-line-length
-     'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-08-how-to-juiciest-turkey-meatballs%2FHow-to-Make-the-Best-Juiciest-Turkey-Meatballs_055'
-     ),
-    new Recipe('Stir Fry',
-    'This is a description for Stir Fry',
-     // tslint:disable-next-line: max-line-length
-     'https://www.momontimeout.com/wp-content/uploads/2018/11/chicken-stir-fry-733x1103.jpg'
-     ),
-    new Recipe('Lasgane',
-    'This is a description for recipe',
-     // tslint:disable-next-line: max-line-length
-     'https://www.jessicagavin.com/wp-content/uploads/2014/02/slice-of-meat-lasagna-600x900.jpg'
-     ),
-  ];
+  recipes: Recipe[];
 
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,27 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe('Meat Balls',
+    'This is a recipe for my famous meat balls',
+     // tslint:disable-next-line: max-line-length
+     'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-08-how-to-juiciest-turkey-meatballs%2FHow-to-Make-the-Best-Juiciest-Turkey-Meatballs_055'
+     ),
+    new Recipe('Stir Fry',
+    'This is a description for Stir Fry',
+     // tslint:disable-next-line: max-line-length
+     'https://www.momontimeout.com/wp-content/uploads/2018/11/chicken-stir-fry-733x1103.jpg'
+     ),
+    new Recipe('Lasgane',
+    'This is a description for recipe',
+     // tslint:disable-next-line: max-line-length
+     'https://www.jessicagavin.com/wp-content/uploads/2014/02/slice-of-meat-lasagna-600x900.jpg'
+     ),
+  ];
+
+getRecipes() {
+  return this.recipes.slice();
+}
+
+}
