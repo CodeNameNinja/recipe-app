6265e331919cd0ff8a0d00a1d79d3fcfe04f6731
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-02-16 12:41:28 +0200

    progmatically navigating to the edit page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 const routes: Routes = [
@@ -13,7 +14,10 @@ const routes: Routes = [
   {path: '', redirectTo: 'recipes', pathMatch: 'full'},
   {path: 'recipes', component: RecipesComponent, children: [
     {path: '', component: RecipeStartComponent},
+    {path: 'new', component: RecipeEditComponent},
     {path: ':id', component: RecipeDetailComponent},
+    {path: ':id/edit', component: RecipeEditComponent},
+
   ]},
   {path: 'shopping-list', component: ShoppingListComponent},
 ];
